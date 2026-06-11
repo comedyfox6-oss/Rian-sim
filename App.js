6564344b533a59de8dbd3af952924e87f1e63374
comedyfox6-oss <comedyfox6@gmail.com>
2026-06-11 23:57:06 +0300

    Update App.js

--- App.js
+++ App.js
@@ -3,6 +3,21 @@ import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, StatusBar } from 'react-native';
 import RianSprite from './RianSprite';
 import { RIAN_STATES, getNextState } from './rianBrain';
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StatusBar } from 'react-native';
+import RianSprite from './RianSprite';
+import { RIAN_STATES, getNextState } from './RianState';
+
+// --- СПРАЙТЫ ---
+import rian_idle from './assets/rian_idle.png';
+import rian_blink from './assets/rian_blink.png';
+import rian_left from './assets/rian_left.png';
+import rian_right from './assets/rian_right.png';
+import rian_jump from './assets/rian_jump.png';
+import rian_jump2 from './assets/rian_jump2.png';
+import rian_sit from './assets/rian_sit.png';
+import rian_think from './assets/rian_think.png';
+
 
 export default function App() {
   const [state, setState] = useState(RIAN_STATES.IDLE);
